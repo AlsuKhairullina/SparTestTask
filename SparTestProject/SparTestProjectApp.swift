04f8b6e386ded9d1b509fa4eda9b9e50e11bc42c
Alsu Khairullina <a.khairullina@astondevs.ru>
2024-08-13 17:37:10 +0500

    Initial Commit

---
+++ SparTestProject/SparTestProjectApp.swift
@@ -0,0 +1,17 @@
+//
+//  SparTestProjectApp.swift
+//  SparTestProject
+//
+//  Created by Алсу Хайруллина on 13.08.2024.
+//
+
+import SwiftUI
+
+@main
+struct SparTestProjectApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
